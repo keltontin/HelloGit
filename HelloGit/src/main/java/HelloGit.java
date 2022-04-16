6512aed1d224eb1f6468a05572a41eef6d90c1a2
keltontin <KeltonTin@163.com>
2022-04-16 11:49:52 +0800

    添加文件

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C), 2020-2022, XDU
+ * FileName: HelloGit
+ * Author: Dingq
+ * Date: 2022/4/16 11:23
+ * Description:
+ */
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
